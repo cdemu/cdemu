e14e1dac70185624a79076d0c2f13e99c631dfe4
Henrik Stokseth <hstokset@users.sourceforge.net>
2008-09-20 17:31:22 +0000

    The good news is that mode 2 form 1 finally works now. The bad news is that someone in the CIF-format design workgroup obviously hit their head severely. *curses*
    old-svn-rev: 443

--- src/parsers/image-cif/image-cif-disc.c
+++ src/parsers/image-cif/image-cif-disc.c
@@ -219,7 +219,7 @@ static gint __mirage_disc_cif_convert_track_mode (MIRAGE_Disc *self, guint32 mod
         }
     } else if(mode == CIF_MODE_MODE2_FORM1) {
         switch(sector_size) {
-            case 2057:
+            case 2056:
                 return MIRAGE_MODE_MODE2_MIXED;
             default: 
                 MIRAGE_DEBUG(self, MIRAGE_DEBUG_WARNING, "%s: unknown sector size %i!\n", __func__, sector_size);
@@ -303,7 +303,8 @@ static gboolean __mirage_disc_cif_parse_track_entries (MIRAGE_Disc *self, GError
 
         /* workaround for mode2/cdrom-xa modes */
         if (track_mode == CIF_MODE_MODE2_FORM1) {
-            real_sector_size = 2057;
+            track_start += 16;
+            track_length -= 16;
         } else if (track_mode == CIF_MODE_MODE2_FORM2) {
             real_sector_size = 2332;
         }
