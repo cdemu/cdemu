f5578ef7d7734bfe08a8dfee19365bab3c08998c
Rok Mandeljc <mrok@users.sourceforge.net>
2009-07-05 21:20:24 +0000

    Daemon: improve daemon/local mode switch a bit...
    old-svn-rev: 620

--- cdemu-daemon/src/main.c
+++ cdemu-daemon/src/main.c
@@ -21,27 +21,68 @@
 #include "cdemud.h"
 
 /* Log handler for daemon */
-static void __cdemud_daemon_daemon_log_handler (const gchar *log_domain, GLogLevelFlags log_level, const gchar *message, gpointer unused_data) {
-    daemon_log(LOG_INFO, message);
+static void __daemon_log_handler (const gchar *log_domain, GLogLevelFlags log_level, const gchar *message, gpointer unused_data) {
+    gint syslog_level;
+
+    switch (log_level) {
+        case G_LOG_LEVEL_ERROR: {
+            syslog_level = LOG_ERR;
+            break;
+        }
+        case G_LOG_LEVEL_CRITICAL: {
+            syslog_level = LOG_CRIT;
+            break;
+        }
+        case G_LOG_LEVEL_WARNING: {
+            syslog_level = LOG_WARNING;
+            break;
+        }
+        case G_LOG_LEVEL_MESSAGE: {
+            syslog_level = LOG_NOTICE;
+            break;
+        }
+        case G_LOG_LEVEL_INFO: {
+            syslog_level = LOG_INFO;
+            break;
+        }
+        case G_LOG_LEVEL_DEBUG: {
+            syslog_level = LOG_DEBUG;
+            break;
+        }
+        default: {
+            syslog_level = LOG_DEBUG;
+            break;
+        }            
+    }
+    
+    daemon_log(syslog_level, "%s", message);
 }
 
 /* Log handler for non-daemon */
-static void __cdemud_daemon_local_log_handler (const gchar *log_domain, GLogLevelFlags log_level, const gchar *message, gpointer unused_data) {
-    g_print(message);
+static void __local_log_handler (const gchar *log_domain, GLogLevelFlags log_level, const gchar *message, gpointer unused_data) {
+    g_print("%s", message);
 }
 
 /* Signal handler */
-static gboolean __cdemud_daemon_signal_handler (GIOChannel *source, GIOCondition condition, gpointer user_data) {
+static gboolean __signal_handler (GIOChannel *source, GIOCondition condition, gpointer user_data) {
     CDEMUD_Daemon *self = CDEMUD_DAEMON(user_data);
-    gint sig;
+    gint signal;
             
     /* Get signal */
-    if ((sig = daemon_signal_next()) <= 0) {
-        cdemud_daemon_stop_daemon(self, NULL);
+    signal = daemon_signal_next();
+
+    if (signal == 0) {
+        /* No signals queued */
+        return TRUE;
+    } else if (signal < 0) {
+        g_warning("Failed to query queued signals (%d)!\n", signal);
+        return TRUE;
     }
-        
+
+    g_message("Received signal - %s\n", g_strsignal(signal));
+    
     /* Dispatch signal */
-    switch (sig) {
+    switch (signal) {
         case SIGINT:
         case SIGQUIT: 
         case SIGTERM:
@@ -79,7 +120,7 @@ static GOptionEntry option_entries[] = {
     { NULL }
 };
 
-static const gchar *__cdemud_custom_pid_file (void) {
+static const gchar *__custom_pid_file_proc (void) {
     return pid_file;
 }
 
@@ -96,16 +137,11 @@ static gboolean __run_daemon () {
         bus = "system";
     }
     
-    g_debug("Starting daemon in %s mode with following parameters:\n"
-            " - num devices: %i\n"
-            " - ctl device: %s\n"
-            " - audio driver: %s\n"
-            " - bus type: %s\n",
-            daemonize ? "daemon" : "local", 
-            num_devices, 
-            ctl_device, 
-            audio_driver, 
-            bus);
+    g_message("Starting daemon in %s mode with following parameters:\n", daemonize ? "daemon" : "local");
+    g_message(" - num devices: %i\n", num_devices);
+    g_message(" - ctl device: %s\n", ctl_device);
+    g_message(" - audio driver: %s\n", audio_driver);
+    g_message(" - bus type: %s\n", bus);
     
     /* Decipher bus type */
     if (!mirage_helper_strcasecmp(bus, "system")) {
@@ -136,7 +172,7 @@ static gboolean __run_daemon () {
             gint signal_fd = daemon_signal_fd();
             GIOChannel *signal_channel = g_io_channel_unix_new(signal_fd);
             
-            g_io_add_watch(signal_channel, G_IO_IN, __cdemud_daemon_signal_handler, CDEMUD_DAEMON(obj));
+            g_io_add_watch(signal_channel, G_IO_IN, __signal_handler, CDEMUD_DAEMON(obj));
             
             /* If ran in daemon mode, send signal to parent */
             if (daemonize) {
@@ -149,7 +185,9 @@ static gboolean __run_daemon () {
                 g_error_free(error);
                 succeeded = FALSE;
             }
-        
+
+            g_message("Stopping daemon.\n");
+
             /* Close signal handling */
             daemon_signal_done();
             g_io_channel_unref(signal_channel);
@@ -178,13 +216,18 @@ static gboolean __run_daemon () {
 int main (int argc, char *argv[]) {    
     gint retval;
     pid_t pid;
+
+    /* Glib and threading initialization */
+    g_type_init();
+    g_thread_init(NULL);
     
+    /* Default log handler is local */
+    g_log_set_default_handler(__local_log_handler, NULL);
+
     /* Glib's commandline parser */
     GError *error = NULL;
     GOptionContext *option_context = NULL;
     gboolean succeeded = FALSE;
-        
-    g_thread_init(NULL);
     
     option_context = g_option_context_new("- CDEmu Daemon");
     g_option_context_add_main_entries(option_context, option_entries, NULL);
@@ -192,7 +235,7 @@ int main (int argc, char *argv[]) {
     g_option_context_free(option_context);
     
     if (!succeeded) {
-        g_print("Failed to parse options: %s\n", error->message);
+        g_warning("Failed to parse options: %s\n", error->message);
         g_error_free(error);
         return -1;
     }
@@ -202,24 +245,20 @@ int main (int argc, char *argv[]) {
     
     /* Override default PID file callback if requested */
     if (pid_file)
-        daemon_pid_file_proc = __cdemud_custom_pid_file;
-    
-    /* Log handler */
-    g_log_set_default_handler(__cdemud_daemon_local_log_handler, NULL);
+        daemon_pid_file_proc = __custom_pid_file_proc;
     
     /* Check if we are called with -k parameter */
     if (daemon_kill) {
-        /* Kill daemon with SIGINT */
-        if ((retval = daemon_pid_file_kill_wait(SIGINT, 5)) < 0) {
-            g_print("Failed to kill daemon.\n");
+        /* Kill daemon with SIGTERM */
+        if ((retval = daemon_pid_file_kill_wait(SIGTERM, 5)) < 0) {
+            g_warning("Failed to kill daemon.\n");
         }
         
         return retval;
     }
     
-    g_type_init();
     if (!libmirage_init(&error)) {
-        g_print("Failed to initialize libMirage: %s!\n", error->message);
+        g_warning("Failed to initialize libMirage: %s!\n", error->message);
         g_error_free(error);
         return -1;
     }
@@ -227,13 +266,10 @@ int main (int argc, char *argv[]) {
     /* Now, either we're called in non-daemon/local or in daemon mode */
     if (daemonize) {
         /* *** Daemon mode *** */
-        
-        /* Log handler */
-        g_log_set_default_handler(__cdemud_daemon_daemon_log_handler, NULL);
-                
+                        
         /* Check that the daemon is not rung twice a the same time */
         if ((pid = daemon_pid_file_is_running()) >= 0) {
-            g_warning("Daemon already running on PID file %u\n", pid);
+            g_warning("Daemon already running with PID %u!\n", pid);
             return -1;
         }
         
@@ -242,33 +278,35 @@ int main (int argc, char *argv[]) {
         daemon_retval_init();
         
         /* Do the fork */
-        if ((pid = daemon_fork()) < 0) {
+        pid = daemon_fork();
+        if (pid < 0) {
             /* Exit on error */
             daemon_retval_done();
             return -1;
         } else if (pid) { /* The parent */        
             /* Wait for 20 seconds for the return value passed from the daemon process */
             if ((retval = daemon_retval_wait(20)) < 0) {
-                g_warning("Could not recieve return value from daemon process.\nCheck the system log for error messages.\n");
+                g_warning("Could not recieve return value from daemon process.\n");
+                g_warning("Check the system log for error messages.\n");
                 return retval;
             }
             
             if (retval) {
-                g_warning("Daemon returned %i.", retval);
+                g_warning("Failed to start daemon (%i).\n", retval);
             } else {
-                g_debug("Daemon returned %i.", retval);
+                g_message("Daemon successfully started.\n");
             }
 
             return retval;
-        } else {           
-            retval = __run_daemon(); 
-            retval -= 1; /* True/False -> 0/-1 */
+        } else {
+            /* Daemon; switch to daemon log handler */
+            g_log_set_default_handler(__daemon_log_handler, NULL);
+
+            retval = __run_daemon() - 1; /* True/False -> 0/-1 */
         }
     } else {
         /* *** Local mode *** */
-        
-        retval = __run_daemon(); 
-        retval -= 1; /* True/False -> 0/-1 */
+        retval = __run_daemon() - 1; /* True/False -> 0/-1 */
     }
     
     libmirage_shutdown(NULL);
