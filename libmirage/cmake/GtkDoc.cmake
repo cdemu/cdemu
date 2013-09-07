422411727020fd3a6786d1a9ea6edeb495cec592
Rok Mandeljc <rok.mandeljc@gmail.com>
2013-09-07 18:26:09 +0200

    libMirage: CMakeLists modularization: moved all filter and parser rules into CMakeLists.txt files located in the corresponding subdirectories. This is less elegant than the for loops we had before, but on the other hand, it allows making every plugin and its dependencies optional.

--- libmirage/cmake/GtkDoc.cmake
+++ libmirage/cmake/GtkDoc.cmake
@@ -7,11 +7,19 @@
 include (CMakeParseArguments)
 include (GNUInstallDirs)
 
-include (Utilities)
-
 find_package (PkgConfig REQUIRED QUIET)
 
 pkg_check_modules (GTKDOC REQUIRED QUIET gtk-doc)
+
+# Converts a CMake list to a string containing elements separated by spaces
+function (TO_LIST_SPACES LIST_NAME OUTPUT_VAR)
+    set (NEW_LIST_SPACE)
+    foreach (ITEM ${${LIST_NAME}})
+        set (NEW_LIST_SPACE "${NEW_LIST_SPACE} ${ITEM}")
+    endforeach ()
+    string (STRIP "${NEW_LIST_SPACE}" NEW_LIST_SPACE)
+    set (${OUTPUT_VAR} "${NEW_LIST_SPACE}" PARENT_SCOPE)
+endfunction ()
 
 function (gtk_doc)
     set (options QUIET VERBOSE)
