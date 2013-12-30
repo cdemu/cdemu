e2edc62588cc92b85cc2d57d4062f65731eb50dd
Rok Mandeljc <rok.mandeljc@gmail.com>
2013-12-30 19:07:54 +0100

    libMirage: ISO Parser: (Re)enabled support for multiple tracks and for loading audio files. Note: for audio files, we strictly require the size of the (uncompressed) audio data to be an integer multiple of 2352 in order for parser to recognise the stream/file as valid!

--- libmirage/src/parsers/image-iso/image-iso-parser.c
+++ libmirage/src/parsers/image-iso/image-iso-parser.c
@@ -21,6 +21,19 @@
 
 #define __debug__ "ISO-Parser"
 
+struct IsoFileInfo
+{
+    gint main_data_size;
+    gint main_data_format;
+    gint track_mode;
+
+    gint subchannel_data_size;
+    gint subchannel_format;
+
+    const gchar *filename;
+    GInputStream *stream;
+};
+
 
 /**********************************************************************\
  *                          Private structure                         *
@@ -30,20 +43,19 @@
 struct _MirageParserIsoPrivate
 {
     MirageDisc *disc;
-
-    gint main_data_size;
-    gint track_mode;
-
-    gint subchannel_data_size;
-    gint subchannel_format;
 };
 
 
-static gboolean mirage_parser_iso_determine_sector_size (MirageParserIso *self, GInputStream *stream, GError **error)
+/**********************************************************************\
+ *                         File identification                        *
+\**********************************************************************/
+static gboolean mirage_parser_iso_determine_sector_size (MirageParserIso *self, struct IsoFileInfo *file_info, GError **error)
 {
     const gint valid_sector_sizes[] = { 2048, 2332, 2336, 2352 };
     const gint data_offset[] = { 0, 0, 0, 16 };
     const gint valid_subchannel_sizes[] = { 0, 16, 96 };
+
+    GInputStream *stream = file_info->stream;
 
     gsize file_length;
 
@@ -55,8 +67,9 @@ static gboolean mirage_parser_iso_determine_sector_size (MirageParserIso *self, 
     }
     file_length = g_seekable_tell(G_SEEKABLE(stream));
 
-    /* Make sure the file is large enough to contain ISO descriptor */
-    if (file_length < 17*2448) {
+    /* Make sure the file is large enough; INF8090 requires a track to
+       be at least four seconds long */
+    if (file_length < 4*75*2048) {
         MIRAGE_DEBUG(self, MIRAGE_DEBUG_IMAGE_ID, "%s: parser cannot handle given image: file is too small!\n", __debug__);
         g_set_error(error, MIRAGE_ERROR, MIRAGE_ERROR_CANNOT_HANDLE, "Parser cannot handle given image: file is too small!");
         return FALSE;
@@ -94,15 +107,27 @@ static gboolean mirage_parser_iso_determine_sector_size (MirageParserIso *self, 
 
             if (!memcmp(buf, mirage_pattern_cd001, sizeof(mirage_pattern_cd001))
                 || !memcmp(buf, mirage_pattern_bea01, sizeof(mirage_pattern_bea01))) {
-                self->priv->main_data_size = valid_sector_sizes[j];
-                self->priv->subchannel_data_size = valid_subchannel_sizes[i];
+                file_info->main_data_size = valid_sector_sizes[j];
+                file_info->subchannel_data_size = valid_subchannel_sizes[i];
+                file_info->main_data_format = MIRAGE_MAIN_DATA;
 
-                MIRAGE_DEBUG(self, MIRAGE_DEBUG_IMAGE_ID, "%s: image is an ISO9660/UDF image, with %d-byte sector data and %d-byte subchannel data\n", __debug__, self->priv->main_data_size, self->priv->subchannel_data_size);
+                MIRAGE_DEBUG(self, MIRAGE_DEBUG_IMAGE_ID, "%s: image is an ISO9660/UDF image, with %d-byte sector data and %d-byte subchannel data\n", __debug__, file_info->main_data_size, file_info->subchannel_data_size);
                 return TRUE;
             } else {
                 MIRAGE_DEBUG(self, MIRAGE_DEBUG_IMAGE_ID, "%s: CD001/BEA01 pattern not found!\n", __debug__);
             }
         }
+    }
+
+    /* Now that we have ruled out any possible combination for a data
+       track, if the stream length is multiple of 2352, we assume that
+       we are dealing with audio track data */
+    if (file_length % 2352 == 0) {
+        MIRAGE_DEBUG(self, MIRAGE_DEBUG_IMAGE_ID, "%s: file size is multiple of 2352; assuming file contains audio track data...\n", __debug__);
+        file_info->main_data_size = 2352;
+        file_info->subchannel_data_size = 0;
+        file_info->main_data_format = MIRAGE_MAIN_AUDIO;
+        return TRUE;
     }
 
     /* No supported combination */
@@ -111,24 +136,26 @@ static gboolean mirage_parser_iso_determine_sector_size (MirageParserIso *self, 
     return FALSE;
 }
 
-static gboolean mirage_parser_iso_determine_track_type (MirageParserIso *self, GInputStream *stream, GError **error)
+static gboolean mirage_parser_iso_determine_track_type (MirageParserIso *self, struct IsoFileInfo *file_info, GError **error)
 {
+    GInputStream *stream = file_info->stream;
+
     /* We try to guess track type from main channel data size */
-    switch (self->priv->main_data_size) {
+    switch (file_info->main_data_size) {
         case 2048: {
             MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "%s: 2048-byte main sector data; assuming Mode 1 track\n", __debug__);
-            self->priv->track_mode = MIRAGE_MODE_MODE1;
+            file_info->track_mode = MIRAGE_MODE_MODE1;
             break;
         }
         case 2332:
         case 2336: {
             MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "%s: 2332/2336-byte main sector data; assuming Mode 2 track\n", __debug__);
-            self->priv->track_mode = MIRAGE_MODE_MODE2_MIXED;
+            file_info->track_mode = MIRAGE_MODE_MODE2_MIXED;
             break;
         }
         case 2352: {
             guint8 buf[16];
-            gsize offset = 16 * (self->priv->main_data_size + self->priv->subchannel_data_size);
+            gsize offset = 16 * (file_info->main_data_size + file_info->subchannel_data_size);
 
             MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "%s: 2352-byte main sector data; determining track type at address 16 (offset %lXh)...\n", __debug__, offset);
 
@@ -144,12 +171,12 @@ static gboolean mirage_parser_iso_determine_track_type (MirageParserIso *self, G
                 return FALSE;
             }
 
-            self->priv->track_mode = mirage_helper_determine_sector_type(buf);
+            file_info->track_mode = mirage_helper_determine_sector_type(buf);
             break;
         }
         default: {
-            MIRAGE_DEBUG(self, MIRAGE_DEBUG_WARNING, "%s: unhalded main sector data size %d!\n", __debug__, self->priv->main_data_size);
-            g_set_error(error, MIRAGE_ERROR, MIRAGE_ERROR_IMAGE_FILE_ERROR, "Unhalded main sector data size %d!", self->priv->main_data_size);
+            MIRAGE_DEBUG(self, MIRAGE_DEBUG_WARNING, "%s: unhalded main sector data size %d!\n", __debug__, file_info->main_data_size);
+            g_set_error(error, MIRAGE_ERROR, MIRAGE_ERROR_IMAGE_FILE_ERROR, "Unhalded main sector data size %d!", file_info->main_data_size);
             return FALSE;
         }
     }
@@ -157,22 +184,24 @@ static gboolean mirage_parser_iso_determine_track_type (MirageParserIso *self, G
     return TRUE;
 }
 
-static gboolean mirage_parser_iso_determine_subchannel_type (MirageParserIso *self, GInputStream *stream, GError **error)
+static gboolean mirage_parser_iso_determine_subchannel_type (MirageParserIso *self, struct IsoFileInfo *file_info, GError **error)
 {
-    switch (self->priv->subchannel_data_size) {
+    GInputStream *stream = file_info->stream;
+
+    switch (file_info->subchannel_data_size) {
         case 0: {
             MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "%s: no subchannel data found!\n", __debug__);
-            self->priv->subchannel_format = 0;
+            file_info->subchannel_format = 0;
             break;
         }
         case 16: {
             MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "%s: 16-byte internal Q subchannel data found!\n", __debug__);
-            self->priv->subchannel_format = MIRAGE_SUBCHANNEL_INT | MIRAGE_SUBCHANNEL_Q16;
+            file_info->subchannel_format = MIRAGE_SUBCHANNEL_INT | MIRAGE_SUBCHANNEL_Q16;
             break;
         }
         case 96: {
             guint8 buf[96];
-            gsize offset = 16 * (self->priv->main_data_size + self->priv->subchannel_data_size) + self->priv->main_data_size;
+            gsize offset = 16 * (file_info->main_data_size + file_info->subchannel_data_size) + file_info->main_data_size;
 
             MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "%s: 96-byte internal PW subchannel data found!\n", __debug__);
             MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "%s: determining whether it is linear or interleaved from subchannel data of sector 16 (offset %lXh)...\n", __debug__, offset);
@@ -196,17 +225,17 @@ static gboolean mirage_parser_iso_determine_subchannel_type (MirageParserIso *se
             guint16 crc = mirage_helper_subchannel_q_calculate_crc(buf+12);
             if ((buf[22] << 8 | buf[23]) == crc) {
                 MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "%s: subchannel data appears to be linear!\n", __debug__);
-                self->priv->subchannel_format = MIRAGE_SUBCHANNEL_INT | MIRAGE_SUBCHANNEL_PW96_LIN;
+                file_info->subchannel_format = MIRAGE_SUBCHANNEL_INT | MIRAGE_SUBCHANNEL_PW96_LIN;
             } else {
                 MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "%s: subchannel data appears to be interleaved!\n", __debug__);
-                self->priv->subchannel_format = MIRAGE_SUBCHANNEL_INT | MIRAGE_SUBCHANNEL_PW96_INT;
+                file_info->subchannel_format = MIRAGE_SUBCHANNEL_INT | MIRAGE_SUBCHANNEL_PW96_INT;
             }
 
             break;
         }
         default: {
-            MIRAGE_DEBUG(self, MIRAGE_DEBUG_WARNING, "%s: unhalded subchannel data size %d!\n", __debug__, self->priv->subchannel_data_size);
-            g_set_error(error, MIRAGE_ERROR, MIRAGE_ERROR_IMAGE_FILE_ERROR, "Unhalded subchannel data size %d!", self->priv->subchannel_data_size);
+            MIRAGE_DEBUG(self, MIRAGE_DEBUG_WARNING, "%s: unhalded subchannel data size %d!\n", __debug__, file_info->subchannel_data_size);
+            g_set_error(error, MIRAGE_ERROR, MIRAGE_ERROR_IMAGE_FILE_ERROR, "Unhalded subchannel data size %d!", file_info->subchannel_data_size);
             return FALSE;
         }
     }
@@ -214,30 +243,11 @@ static gboolean mirage_parser_iso_determine_subchannel_type (MirageParserIso *se
     return TRUE;
 }
 
-static gboolean mirage_parser_iso_load_track (MirageParserIso *self, GInputStream *stream, GError **error)
+static gboolean mirage_parser_iso_load_track (MirageParserIso *self, const struct IsoFileInfo *file_info, gboolean add_pregap, GError **error)
 {
     MirageSession *session;
     MirageTrack *track;
     MirageFragment *fragment;
-
-    MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "%s: loading track...\n", __debug__);
-
-    /* Create data fragment */
-    MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "%s: creating data fragment\n", __debug__);
-    fragment = g_object_new(MIRAGE_TYPE_FRAGMENT, NULL);
-
-    mirage_fragment_main_data_set_stream(fragment, stream);
-    mirage_fragment_main_data_set_size(fragment, self->priv->main_data_size);
-    mirage_fragment_main_data_set_format(fragment, MIRAGE_MAIN_DATA);
-    mirage_fragment_subchannel_data_set_size(fragment, self->priv->subchannel_data_size);
-    mirage_fragment_subchannel_data_set_format(fragment, self->priv->subchannel_format);
-
-    /* Use whole file */
-    if (!mirage_fragment_use_the_rest_of_file(fragment, error)) {
-        MIRAGE_DEBUG(self, MIRAGE_DEBUG_WARNING, "%s: failed to use the rest of file!\n", __debug__);
-        g_object_unref(fragment);
-        return FALSE;
-    }
 
     /* Add track */
     MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "%s: adding track\n", __debug__);
@@ -249,15 +259,41 @@ static gboolean mirage_parser_iso_load_track (MirageParserIso *self, GInputStrea
     g_object_unref(session);
 
     /* Set track mode */
-    mirage_track_set_mode(track, self->priv->track_mode);
+    mirage_track_set_mode(track, file_info->track_mode);
 
-    /* Add fragment to track */
+    /* Create pregap fragment */
+    if (add_pregap) {
+        MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "%s: creating pregap fragment\n", __debug__);
+        fragment = g_object_new(MIRAGE_TYPE_FRAGMENT, NULL);
+
+        mirage_fragment_set_length(fragment, 150);
+        mirage_track_add_fragment(track, -1, fragment);
+
+        g_object_unref(fragment);
+    }
+
+    /* Create data fragment */
+    MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "%s: creating data fragment\n", __debug__);
+    fragment = g_object_new(MIRAGE_TYPE_FRAGMENT, NULL);
+
+    mirage_fragment_main_data_set_stream(fragment, file_info->stream);
+    mirage_fragment_main_data_set_size(fragment, file_info->main_data_size);
+    mirage_fragment_main_data_set_format(fragment, file_info->main_data_format);
+    mirage_fragment_subchannel_data_set_size(fragment, file_info->subchannel_data_size);
+    mirage_fragment_subchannel_data_set_format(fragment, file_info->subchannel_format);
+
+    /* Use whole file */
+    if (!mirage_fragment_use_the_rest_of_file(fragment, error)) {
+        MIRAGE_DEBUG(self, MIRAGE_DEBUG_WARNING, "%s: failed to use the rest of file!\n", __debug__);
+        g_object_unref(fragment);
+        g_object_unref(track);
+        return FALSE;
+    }
+
     mirage_track_add_fragment(track, -1, fragment);
 
     g_object_unref(fragment);
     g_object_unref(track);
-
-    MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "%s: finished loading track\n", __debug__);
 
     return TRUE;
 }
@@ -269,70 +305,90 @@ static gboolean mirage_parser_iso_load_track (MirageParserIso *self, GInputStrea
 static MirageDisc *mirage_parser_iso_load_image (MirageParser *_self, GInputStream **streams, GError **error)
 {
     MirageParserIso *self = MIRAGE_PARSER_ISO(_self);
-    const gchar *iso_filename;
-    GInputStream *stream;
+    struct IsoFileInfo *file_info;
+    const gchar **filenames;
+    gint num_files;
+
     gboolean succeeded = TRUE;
 
-    /* Check if file can be loaded */
-    stream = streams[0];
-    g_object_ref(stream);
-    iso_filename = mirage_contextual_get_file_stream_filename(MIRAGE_CONTEXTUAL(self), stream);
+    /* Determine number of streams */
+    for (num_files = 0; streams[num_files]; num_files++);
+    file_info = g_new0(struct IsoFileInfo, num_files);
+    filenames = g_new0(const gchar *, num_files + 1);
 
+    /* Check if all files can be loaded */
     MIRAGE_DEBUG(self, MIRAGE_DEBUG_IMAGE_ID, "%s: checking if parser can handle given image...\n", __debug__);
-    if (!mirage_parser_iso_determine_sector_size(self, stream, error)) {
-        g_object_unref(stream);
-        return FALSE;
+    for (gint i = 0; i < num_files; i++) {
+        file_info[i].stream = streams[i];
+        filenames[i] = mirage_contextual_get_file_stream_filename(MIRAGE_CONTEXTUAL(self), streams[i]);
+
+        MIRAGE_DEBUG(self, MIRAGE_DEBUG_IMAGE_ID, "%s: checking file #%d: '%s'\n", __debug__, i, filenames[i]);
+        if (!mirage_parser_iso_determine_sector_size(self, &file_info[i], error)) {
+            g_free(file_info);
+            g_free(filenames);
+            return FALSE;
+        }
     }
+
     MIRAGE_DEBUG(self, MIRAGE_DEBUG_IMAGE_ID, "%s: parser can handle given image!\n", __debug__);
 
     MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "%s: parsing the image...\n", __debug__);
-
-    MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "%s: ISO filename: %s\n", __debug__, iso_filename);
-
-    /* Determine track and subchannel mode */
-    if (!mirage_parser_iso_determine_track_type(self, stream, error)) {
-        succeeded = FALSE;
-        goto end;
-    }
-    if (!mirage_parser_iso_determine_subchannel_type(self, stream, error)) {
-        succeeded = FALSE;
-        goto end;
-    }
 
     /* Create disc */
     self->priv->disc = g_object_new(MIRAGE_TYPE_DISC, NULL);
     mirage_object_set_parent(MIRAGE_OBJECT(self->priv->disc), self);
 
-    /* Set filenames */
-    mirage_disc_set_filename(self->priv->disc, iso_filename);
-
     /* Session: one session (with possibly multiple tracks) */
     MirageSession *session = g_object_new(MIRAGE_TYPE_SESSION, NULL);
     mirage_disc_add_session_by_index(self->priv->disc, 0, session);
 
-    /* ISO image parser assumes single-track image, so we're dealing with regular CD-ROM session */
+    /* ISO image parser assumes single-session image, so we're dealing with regular CD-ROM session */
     mirage_session_set_session_type(session, MIRAGE_SESSION_CD_ROM);
     g_object_unref(session);
 
-    /* Load track */
-    if (!mirage_parser_iso_load_track(self, stream, error)) {
-        MIRAGE_DEBUG(self, MIRAGE_DEBUG_WARNING, "%s: failed to load track!\n", __debug__);
-        succeeded = FALSE;
-        goto end;
+    /* Go over all files and create tracks */
+    for (gint i = 0; i < num_files; i++) {
+        MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "\n");
+        MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "%s: file #%d: '%s'\n", __debug__, i, filenames[i]);
+
+        /* Determine track and subchannel mode */
+        if (!mirage_parser_iso_determine_track_type(self, &file_info[i], error)) {
+            succeeded = FALSE;
+            break;
+        }
+        if (!mirage_parser_iso_determine_subchannel_type(self, &file_info[i], error)) {
+            succeeded = FALSE;
+            break;
+        }
+
+        /* Load track; in the complete absence of any relevant information,
+           we construct the image in TAO mode (pregaps between tracks) */
+        if (!mirage_parser_iso_load_track(self, &file_info[i], i > 0, error)) {
+            MIRAGE_DEBUG(self, MIRAGE_DEBUG_WARNING, "%s: failed to load track!\n", __debug__);
+            succeeded = FALSE;
+            break;
+        }
     }
 
-    MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "\n");
-    MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "%s: finishing the layout\n", __debug__);
+    /* Finishing touch */
+    if (succeeded) {
+        /* Set filenames */
+        mirage_disc_set_filenames(self->priv->disc, filenames);
 
-    /* Now guess medium type and if it's a CD-ROM, add Red Book pregap */
-    gint medium_type = mirage_parser_guess_medium_type(MIRAGE_PARSER(self), self->priv->disc);
-    mirage_disc_set_medium_type(self->priv->disc, medium_type);
-    if (medium_type == MIRAGE_MEDIUM_CD) {
-        mirage_parser_add_redbook_pregap(MIRAGE_PARSER(self), self->priv->disc);
+        MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "\n");
+        MIRAGE_DEBUG(self, MIRAGE_DEBUG_PARSER, "%s: finishing the layout\n", __debug__);
+
+        /* Now guess medium type and if it's a CD-ROM, add Red Book pregap */
+        gint medium_type = mirage_parser_guess_medium_type(MIRAGE_PARSER(self), self->priv->disc);
+        mirage_disc_set_medium_type(self->priv->disc, medium_type);
+        if (medium_type == MIRAGE_MEDIUM_CD) {
+            mirage_parser_add_redbook_pregap(MIRAGE_PARSER(self), self->priv->disc);
+        }
     }
 
-end:
-    g_object_unref(stream);
+    /* Cleanup */
+    g_free(file_info);
+    g_free(filenames);
 
     /* Return disc */
     if (succeeded) {
@@ -364,8 +420,9 @@ static void mirage_parser_iso_init (MirageParserIso *self)
     mirage_parser_generate_info(MIRAGE_PARSER(self),
         "PARSER-ISO",
         "ISO Image Parser",
-        1,
-        "ISO images (*.iso, *.bin, *.img)", "application/x-cd-image"
+        2,
+        "ISO images (*.iso, *.bin, *.img)", "application/x-cd-image",
+        "WAV audio files (*.wav)", "audio/x-wav"
     );
 }
 
