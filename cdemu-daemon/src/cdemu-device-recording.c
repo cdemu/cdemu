5aff5dda5245529e828d52a9db6fd3988db8d645
Rok Mandeljc <rok.mandeljc@gmail.com>
2014-02-08 18:54:53 +0100

    Daemon: device: recording: when session is closed and disc finalized, emit 'status-changed' signal to allow clients refresh their image file list.

--- cdemu-daemon/src/cdemu-device-recording.c
+++ cdemu-daemon/src/cdemu-device-recording.c
@@ -129,6 +129,9 @@ static gboolean cdemu_device_recording_close_session (CdemuDevice *self)
             self->priv->disc_closed = TRUE;
 
             mirage_writer_finalize_image(self->priv->image_writer, self->priv->disc, NULL);
+
+            /* Send notification */
+            g_signal_emit_by_name(self, "status-changed", NULL);
         }
 
         self->priv->num_written_sectors = 0; /* Reset */
