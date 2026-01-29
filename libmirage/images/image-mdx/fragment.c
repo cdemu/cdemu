b98253429ff891e0b880961fdb33d59d1bf6c141
Rok Mandeljc <rok.mandeljc@gmail.com>
2026-01-29 23:08:46 +0100

    libMirage: MDX fragment: fix bug in tweak counter computation
    When computing sector address that is used as a base for initial
    value of tweak counter for LRW decryption, make sure to use 64-bit
    integer instead of 32-bit one. Otherwise, once multiplied by
    sector size, the value might overflow (at sufficiently high
    sector address) and result in invalid/mismatched tweak counter
    value.

--- libmirage/images/image-mdx/fragment.c
+++ libmirage/images/image-mdx/fragment.c
@@ -503,10 +503,10 @@ static gboolean mirage_fragment_mdx_read_sector_data (MirageFragmentMdx *self, g
             const gsize aligned_data_len = read_len & ~15;
             const guint aligned_sector_size = sector_size & ~15;
 
-            const gint start_sector_address = sector_group * self->priv->sectors_in_group;
+            const guint64 start_sector_address = sector_group * self->priv->sectors_in_group;
             const guint64 tweak_counter = 1 + start_sector_address * aligned_sector_size / 16;
 
-            MIRAGE_DEBUG(self, MIRAGE_DEBUG_FRAGMENT, "%s: decrypting data with starting sector number %d, aligned data length is %" G_GINT64_MODIFIER "d, aligned sector size is %d, start value of tweak counter is %" G_GINT64_MODIFIER "d\n", __debug__, start_sector_address, aligned_data_len, aligned_sector_size, tweak_counter);
+            MIRAGE_DEBUG(self, MIRAGE_DEBUG_FRAGMENT, "%s: decrypting data with starting sector number %" G_GINT64_MODIFIER "d, aligned data length is %" G_GINT64_MODIFIER "d, aligned sector size is %d, start value of tweak counter is %" G_GINT64_MODIFIER "d\n", __debug__, start_sector_address, aligned_data_len, aligned_sector_size, tweak_counter);
 
             gboolean succeeded = mdx_crypto_decipher_buffer_lrw(
                 self->priv->crypt_handle,
